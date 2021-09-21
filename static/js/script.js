c81a229c9fa8136e89df73dfaa0d8fda2090e1e1
Jocelyn Tang <jocelyn.tang@hackbrightacademy.com>
2021-09-21 18:22:19 +0000

    update script.js for if there's no availability

--- static/js/script.js
+++ static/js/script.js
@@ -7,13 +7,18 @@ $('#schedule').on('submit', (evt) => {
     };
 
     $.post("/search_reservations", formData, (res) => {
-        $('#available_reservations').html("Below is the current availability. Select a time that works for you!");
-        for (time of res) {
-            $('#available_reservations').append(
-                `<form action="/reservations/book" method="POST">
-                <input value="${time}" name='start_time' type='submit' class='schedule_res'>
-                </form>`);
-        }
+         if (res.length === 0) {
+            $('#available_reservations').html("Sorry, there is no availability at these times, try another search :(")
+         }
+         else {
+            $('#available_reservations').html("Below is the current availability. Select a time that works for you!");
+            for (time of res) {
+                $('#available_reservations').append(
+                    `<form action="/reservations/book" method="POST">
+                    <input value="${time}" name='start_time' type='submit' class='schedule_res'>
+                    </form>`);
+            }
+         }
     })
 
 })
